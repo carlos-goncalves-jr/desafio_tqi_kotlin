b48aac303bb9afe94378e5469d55dacbf70853f3
ogQoda <carlos_ojr@live.com>
2023-07-26 18:06:05 -0300

    Criação do projeto SpringBoot.

---
+++ src/main/kotlin/com/acme/desafiotqi/DesafioTqiApplication.kt
@@ -0,0 +1,13 @@
+package com.acme.desafiotqi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class DesafioTqiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioTqiApplication>(*args)
+}
---
+++ src/test/kotlin/com/acme/desafiotqi/DesafioTqiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acme.desafiotqi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioTqiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
