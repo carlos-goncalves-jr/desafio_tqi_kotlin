186e6043c27a4176c7a9cc7941e737f005dea05a
ogQoda <carlos_ojr@live.com>
2023-08-03 17:02:55 -0300

    Refatoração dos testes da classe CategoriaRepository

--- src/test/kotlin/com/acme/desafiotqi/repository/CategoriaRepositoryTest.kt
+++ src/test/kotlin/com/acme/desafiotqi/repository/CategoriaRepositoryTest.kt
@@ -2,24 +2,38 @@ package com.acme.desafiotqi.repository
 
 import com.acme.desafiotqi.model.Categoria
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CategoriaRepositoryTest {
 
     @Autowired
     lateinit var categoriaRepository: CategoriaRepository
+    lateinit var categoria : Categoria
+    lateinit var categoria2 : Categoria
+    lateinit var categoria3 : Categoria
+    lateinit var categoria4 : Categoria
+    lateinit var listaDeCategorias: List<Categoria>
+
+    @BeforeAll
+    fun setUpTests(){
+        categoria = Categoria(nome = "Bebidas")
+        categoria2 = Categoria(nome = "Peixaria")
+        categoria3 = Categoria(nome = "Hortifruti")
+        categoria4 = Categoria(nome = "Padaria")
+        listaDeCategorias = categoriaRepository.saveAll(listOf(categoria, categoria2, categoria3, categoria4))
+    }
 
     @Test
     fun categoriaRepositoryExistsByNomeTest() {
-        val categoria = Categoria(nome = "Bebidas")
-        categoriaRepository.save(categoria)
-
-        val existsSavedCategoria: Boolean  = categoriaRepository.existsByNome(categoria.nome)
+        val existsSavedCategoria: Boolean  = categoriaRepository.existsByNome(nome = "Bebidas")
 
         assertThat(existsSavedCategoria).isNotNull
         assertThat(existsSavedCategoria).isTrue()
@@ -27,27 +41,22 @@ class CategoriaRepositoryTest {
 
     @Test
     fun categoriaRepositoryFindByNomeTest() {
-        val categoria = Categoria(nome = "Padaria")
-        categoriaRepository.save(categoria)
-
-        val savedCategoria: Categoria = categoriaRepository.findByNome(categoria.nome)
+        val savedCategoria: Categoria = categoriaRepository.findByNome(nome = "Peixaria")
 
         assertThat(savedCategoria).isNotNull()
         assertThat(savedCategoria.id).isGreaterThan(0)
-        assertThat(savedCategoria.nome).isEqualTo("Padaria")
+        assertThat(savedCategoria.nome).isEqualTo("Peixaria")
     }
 
     @Test
     fun categoriaRepositoryFindAllTest(){
-        val categoria  = Categoria(nome = "Padaria")
-        val categoria2 = Categoria(nome = "Peixaria")
-        val categoria3 = Categoria(nome = "Hortifruti")
-        val categoria4 = Categoria(nome = "Hortifruti")
-
-        val listaDeCategorias: List<Categoria> = categoriaRepository.saveAll(listOf(categoria, categoria2, categoria3, categoria4))
-
         assertThat(listaDeCategorias.size).isEqualTo(4)
         assertThat(listaDeCategorias).isNotNull
+    }
+
+    @Test
+    fun categoriaRepositoryCheckId(){
+        assertThat(categoriaRepository.findByNome("Padaria").id).isNotNull()
     }
 
 }
